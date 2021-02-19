3a205f943ae4feae9f75c3452de1e5cf6d46f115
Brian Cai <thebriancai@gmail.com>
2021-02-19 21:54:38 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -3,7 +3,7 @@ module "s3-bucket" {
   version = "1.15.0"
   # insert the 5 required variables here
   acceleration_status = "Enabled"
-  bucket = "brian-bucket"
+  bucket = ""
   bucket_prefix = "brian-cai"
   policy = "{}"
   request_payer = "BucketOwner"
