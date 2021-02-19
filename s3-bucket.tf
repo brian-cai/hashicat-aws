cd9f8bdfdcb221b6b0c1142a22ce69e245e02b47
Brian Cai <thebriancai@gmail.com>
2021-02-19 21:46:23 +0000

    Added s3 bucket module

---
+++ s3-bucket.tf
@@ -0,0 +1,10 @@
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "1.17.0"
+  # insert the 5 required variables here
+  acceleration_status = "Enabled"
+  bucket = "brian-bucket"
+  bucket_prefix = "brian-cai"
+  policy = {}
+  request_payer = "BucketOwner"
+}
